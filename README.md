dd26e0dda29376286f8ee70e7974550c98f84560
Hammer1279 <tobias.lenhardt@uni-leipzig.de>
2025-02-10 17:18:21 +0100

    fixes and QoL improvements, debugging tools added

--- README.md
+++ README.md
@@ -1,6 +1,10 @@
 # node-telnet-client [![Node.js CI](https://github.com/Hammer1279/node-telnet-client/actions/workflows/pkg.yml/badge.svg)](https://github.com/Hammer1279/node-telnet-client/actions/workflows/pkg.yml)
 
-To be used with HTDev BBS Server for encrypted communication and extended functionality.
+A zero dependency NodeJS Implementation of a [RFC-854](https://datatracker.ietf.org/doc/html/rfc854) telnet client.
+
+This is a very early version that so far isn't very compatible with most major telnet servers yet.
+
+It is developed for the [HTDev Telnet Server](telnet://ht-dev.de) for encrypted communication and extended functionality.
 
 ## Autorun command
 
@@ -16,3 +20,8 @@ For experimental versions, check the pre-releases under releases, for "nightly" 
 It has zero dependencies itself so to run a copy locally, just run it with `node index.js`
 
 To build the application, run `npm install` and then `npm run build`.
+
+## Build Dependencies
+
+- @yao-pkg/pkg: Compiling the Application
+- protocol-registry: Register the telnet uri as per [RFC-4248](https://datatracker.ietf.org/doc/html/rfc4248)
--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+// rework to use chunkermaster
+
 const fs = require("fs");
 const path = require("path");
 const { createConnection } = require('net');
@@ -12,6 +14,7 @@ const encryptionDelay = 200; // additional delay for encryption
 const initializationDelay = 500; // additional delay for initialization
 
 const advancedFeatures = true; // Enable advanced features
+const printIO = false; // Print input/output data
 
 class MemoryStream extends Transform {
     constructor(options = {}) {
@@ -24,8 +27,46 @@ class MemoryStream extends Transform {
     }
 }
 
+/**
+ * prepare the session and close handler when done
+ * @param {Buffer} data
+ * @returns {Buffer[]} chunks
+ */
+function chunkData(data) {
+    // Split data into chunks based on IAC
+    const chunks = [];
+    let currentChunk = Buffer.alloc(0);
+
+    for (let i = 0; i < data.length; i++) {
+        if (data[i] === IAC[0]) {
+            if (currentChunk.length > 0) {
+                chunks.push(currentChunk);
+            }
+            // Find the end of the IAC command
+            let cmdLength = 3; // Default IAC command length
+            if (data[i + 1] === SB[0]) {
+                // Find SE to end subnegotiation
+                const seIndex = data.indexOf(SE[0], i);
+                cmdLength = seIndex - i + 1;
+            }
+            chunks.push(data.subarray(i, i + cmdLength));
+            i += cmdLength - 1;
+            currentChunk = Buffer.alloc(0);
+        } else {
+            currentChunk = Buffer.concat([currentChunk, Buffer.from([data[i]])]);
+        }
+    }
+
+    if (currentChunk.length > 0) {
+        chunks.push(currentChunk);
+    }
+
+    return chunks;
+}
+
 const writer = new MemoryStream();
 let initialized = false; // Initialization status, do not modify directly, runtime only
+let handoffInit = false; // Handoff initialization status to server, do not modify directly, runtime only
 let encrypted = false; // Encryption status, do not modify directly, runtime only
 let privateKey; // Private key, do not modify directly, runtime only
 let keyCurve; // Key curve, do not modify directly, runtime only
@@ -86,6 +127,42 @@ const PAUSE = Buffer.from([0x86]); // Pause
 const RESUME = Buffer.from([0x87]); // Resume
 const START_CONTENT = Buffer.from([0x88]); // Start Content
 const END_CONTENT = Buffer.from([0x89]); // End Content
+
+// ANSI escape codes for colors
+const ANSI = {
+    // Reset
+    reset: '\x1b[0m',
+
+    // Basic foreground colors
+    black: '\x1b[30m',
+    red: '\x1b[31m',
+    green: '\x1b[32m',
+    yellow: '\x1b[33m',
+    blue: '\x1b[34m',
+    magenta: '\x1b[35m',
+    cyan: '\x1b[36m',
+    white: '\x1b[37m',
+
+    // Bright foreground colors
+    brightBlack: '\x1b[90m',
+    brightRed: '\x1b[91m',
+    brightGreen: '\x1b[92m',
+    brightYellow: '\x1b[93m',
+    brightBlue: '\x1b[94m',
+    brightMagenta: '\x1b[95m',
+    brightCyan: '\x1b[96m',
+    brightWhite: '\x1b[97m',
+
+    // Basic background colors
+    bgBlack: '\x1b[40m',
+    bgRed: '\x1b[41m',
+    bgGreen: '\x1b[42m',
+    bgYellow: '\x1b[43m',
+    bgBlue: '\x1b[44m',
+    bgMagenta: '\x1b[45m',
+    bgCyan: '\x1b[46m',
+    bgWhite: '\x1b[47m'
+};
 
 // Encrypt
 function encrypt(plaintext, key = privateKey, encoding = 'utf8') {
@@ -154,7 +231,9 @@ process.stdin.resume();
 // format keys before sending to server
 process.stdin.on('data', async (key) => {
     if (key === '\u0004') { // Ctrl+D
-        process.exit();
+        socket.end(); // End connection
+        socket.destroy(); // Destroy socket
+        process.exit(); // Exit process
     } else if (key === '\u0003') { // Ctrl+C
         send(IP); // Interrupt Process
     } else if (key === '\u0018') { // Ctrl+X
@@ -185,6 +264,7 @@ process.stdin.on('data', async (key) => {
             process.stdout.write("> ");
         } else if (command == "disconnect") {
             send(IP); // Interrupt Process
+            socket.end(); // End connection
         } else if (command == "status") {
             console.log("Client status:");
             console.log("Host:", hostname + ":" + port);
@@ -195,6 +275,8 @@ process.stdin.on('data', async (key) => {
             // console.log("Public Key:", key.getPublicKey() ? key.getPublicKey().toString("hex") : "null");
             // console.log("Private Key:", privateKey ? privateKey.toString("hex") : "null");
             process.stdout.write("$ ");
+        } else if (command == "debug") {
+            socket.write(Buffer.concat([IAC, DO, STATUS])); // Debug request Status
         } else if (command == "keyinfo") {
             console.log("Key Information:");
             console.log("Key Curve:", keyCurve);
@@ -206,6 +288,7 @@ process.stdin.on('data', async (key) => {
             // Add any additional functionality for client side commands
             console.log("Client commands:");
             console.log("status - Show client status");
+            console.log("debug - [DEV] Request server status");
             console.log("keyinfo - Show key information");
             console.log("disconnect - Disconnect from server");
             console.log("exit - Exit client command mode");
@@ -217,67 +300,86 @@ process.stdin.on('data', async (key) => {
 });
 
 socket.on("data", (data) => {
-    if (!encrypted || !initialized) {
-        if (data.equals(PAUSE)) {
-            process.stdin.pause();
-        } else if (data.equals(RESUME)) {
-            process.stdin.resume();
-        } else if (data.equals(IP)) {
-            process.exit();
-        } else if (data.includes(Buffer.concat([IAC, SB, CUSTOM_CLIENT_INIT, NUL]))) {
-            const offsetBegin = data.indexOf(SB) + 2;
-            const offsetEnd = data.lastIndexOf(SE) - 1;
-            const algorithmData = data.subarray(offsetBegin + 1, offsetEnd);
-            keyCurve = algorithmData.toString('utf8');
-            echdKey = createECDH(keyCurve);
-            console.debug("Using key curve:", keyCurve);
-        } else if (data.equals(Buffer.concat([IAC, DO, CUSTOM_CLIENT_INIT]))) {
-            // server supports custom client features
-            socket.write(Buffer.concat([IAC, WILL, KEY_EXCHANGE])); // Start Key Exchange
-        } else if (data.includes(Buffer.concat([IAC, DO, KEY_EXCHANGE]))) {
-            // generate keys
-            const publicKey = echdKey.generateKeys();
-            console.debug("Generated Key: " + publicKey.toString("hex"));
-        } else if (data.equals(Buffer.concat([IAC, SB, KEY_EXCHANGE, ONE /* value required */, IAC, SE]))) {
-            socket.write(Buffer.concat([IAC, SB, KEY_EXCHANGE, NUL, echdKey.getPublicKey(), IAC, SE]));
-            // send key to server
-        } else if (data.includes(Buffer.concat([IAC, SB, KEY_EXCHANGE, NUL /* value provided */]))) {
-            // server sent its key, generate secret
-            console.debug("Key exchange received");
-            const offsetBegin = data.indexOf(SB) + 2;
-            const offsetEnd = data.lastIndexOf(SE) - 1;
-            const keyData = data.subarray(offsetBegin + 1, offsetEnd); // client public key
-            console.log("Extracted key:", keyData.toString("hex"));
-            privateKey = echdKey.computeSecret(keyData);
-            socket.write(Buffer.concat([IAC, WILL, ENCRYPTION])); // Enable Encryption
-        } else if (data.includes(Buffer.concat([IAC, SB, ENCRYPTION, NUL]))) {
-            const offsetBegin = data.indexOf(SB) + 2;
-            const offsetEnd = data.lastIndexOf(SE) - 1;
-            const algorithmData = data.subarray(offsetBegin + 1, offsetEnd);
-            encryptionAlgorithm = algorithmData.toString('utf8');
-            console.debug("Using encryption algorithm:", encryptionAlgorithm);
-        } else if (data.equals(Buffer.concat([IAC, DO, ENCRYPTION]))) {
-            // enable encryption
-            encrypted = true;
-            console.debug("Encryption enabled");
-            console.debug("Private Key: " + privateKey.toString("hex"));
-        } else if (!hold) {
-            process.stdout.write(data);
-        } else {
-            // console.debug("Data on hold");
-            holdBuffer += data.toString();
+    const chunks = chunkData(data);
+    for (const chunk of chunks) {
+        if (printIO) {
+            console.debug("Received chunk:", chunk.toString("hex"));
         }
-    } else {
-        // decrypt data
-        try {
-            const decryptedData = decrypt(data, privateKey);
-            process.stdout.write(decryptedData);
-        } catch (error) {
-            console.error("Decryption error:", error);
-            console.error("Data:", data.toString("hex"));
-            console.debug("Algorithm:", encryptionAlgorithm);
-            console.debug("Private Key:", privateKey.toString("hex"));
-            console.debug("Public Key:", echdKey.getPublicKey().toString("hex"));
+        if (!encrypted || !initialized) {
+            if (chunk.equals(PAUSE)) {
+                process.stdin.pause();
+            } else if (chunk.equals(RESUME)) {
+                process.stdin.resume();
+            } else if (chunk.equals(IP)) {
+                process.exit();
+            } else if (chunk.includes(Buffer.concat([IAC, SB, CUSTOM_CLIENT_INIT, NUL]))) {
+                const offsetBegin = chunk.indexOf(SB) + 2;
+                const offsetEnd = chunk.lastIndexOf(SE) - 1;
+                const algorithmData = chunk.subarray(offsetBegin + 1, offsetEnd);
+                keyCurve = algorithmData.toString('utf8');
+                echdKey = createECDH(keyCurve);
+                console.debug("Using key curve:", keyCurve);
+            } else if (chunk.equals(Buffer.concat([IAC, DO, CUSTOM_CLIENT_INIT]))) {
+                // server supports custom client features
+                socket.write(Buffer.concat([IAC, WILL, KEY_EXCHANGE])); // Start Key Exchange
+            } else if (chunk.includes(Buffer.concat([IAC, DO, KEY_EXCHANGE]))) {
+                // generate keys
+                const publicKey = echdKey.generateKeys();
+                console.debug("Generated Key: " + publicKey.toString("hex"));
+            } else if (chunk.equals(Buffer.concat([IAC, SB, KEY_EXCHANGE, ONE /* value required */, IAC, SE]))) {
+                socket.write(Buffer.concat([IAC, SB, KEY_EXCHANGE, NUL, echdKey.getPublicKey(), IAC, SE]));
+                // send key to server
+            } else if (chunk.includes(Buffer.concat([IAC, SB, KEY_EXCHANGE, NUL /* value provided */]))) {
+                // server sent its key, generate secret
+                console.debug("Key exchange received");
+                const offsetBegin = chunk.indexOf(SB) + 2;
+                const offsetEnd = chunk.lastIndexOf(SE) - 1;
+                const keyData = chunk.subarray(offsetBegin + 1, offsetEnd); // client public key
+                console.log("Extracted key:", keyData.toString("hex"));
+                privateKey = echdKey.computeSecret(keyData);
+                socket.write(Buffer.concat([IAC, WILL, ENCRYPTION])); // Enable Encryption
+            } else if (chunk.includes(Buffer.concat([IAC, SB, STATUS, NUL /* value provided */]))) {
+                // server sent status
+                const offsetBegin = chunk.indexOf(SB) + 2;
+                const offsetEnd = chunk.lastIndexOf(SE) - 1;
+                const statusData = chunk.subarray(offsetBegin + 1, offsetEnd);
+                console.debug("Server status:", require("util").inspect(JSON.parse(statusData.toString('utf8'))));
+            } else if (chunk.includes(Buffer.concat([IAC, SB, ENCRYPTION, NUL]))) {
+                const offsetBegin = chunk.indexOf(SB) + 2;
+                const offsetEnd = chunk.lastIndexOf(SE) - 1;
+                const algorithmData = chunk.subarray(offsetBegin + 1, offsetEnd);
+                encryptionAlgorithm = algorithmData.toString('utf8');
+                console.debug("Using encryption algorithm:", encryptionAlgorithm);
+            } else if (chunk.equals(Buffer.concat([IAC, DO, ENCRYPTION]))) {
+                // enable encryption
+                encrypted = true;
+                console.debug("Encryption enabled");
+                console.debug("Private Key: " + privateKey.toString("hex"));
+            } else if (chunk.equals(Buffer.concat([IAC, DO, Buffer.from([0x80])]))) {
+                handoffInit = true;
+                console.debug("Handoff initialization complete");
+            } else if (chunk.equals(Buffer.concat([IAC, DO, Buffer.from([0x80])]))) { // Initialize
+                socket.write(Buffer.concat([IAC, WILL, Buffer.from([0x80])]));
+                initialized = true;
+                console.debug("Initialization complete");
+            } else if (!hold) {
+                process.stdout.write(chunk);
+            } else {
+                // console.debug("Data on hold");
+                holdBuffer += chunk.toString();
+            }
+        } else {
+            // decrypt data
+            try {
+                const decryptedData = decrypt(chunk, privateKey);
+                process.stdout.write(decryptedData);
+            } catch (error) {
+                console.error("Decryption error:", error);
+                console.error("Data:", chunk.toString("hex"));
+                console.debug("Algorithm:", encryptionAlgorithm);
+                console.debug("Private Key:", privateKey.toString("hex"));
+                console.debug("Public Key:", echdKey.getPublicKey().toString("hex"));
+            }
         }
     }
 });
@@ -301,7 +403,7 @@ socket.on("connect", async () => {
     await delay(delayMs); // Wait for server to process
     socket.write(Buffer.concat([IAC, WILL, TERMINAL_TYPE, IAC, SB, TERMINAL_TYPE, NUL, Buffer.from("xterm-256color"), IAC, SE])); // Terminal Type
     await delay(delayMs);
-    socket.write(Buffer.concat([IAC, DO, ECHO])); // Echo
+    socket.write(Buffer.concat([IAC, DO, ECHO])); // Tell server to echo input
     await delay(delayMs);
     if (advancedFeatures) {
         socket.write(Buffer.concat([IAC, WILL, CUSTOM_CLIENT_INIT])); // Custom Client Initialization
@@ -313,14 +415,19 @@ socket.on("connect", async () => {
     }
     await delay(initializationDelay); // Wait for server to process
     await send(""); // Send empty command to initialize connection
-    initialized = true;
-    console.debug("Initialization complete");
+
+    if (!handoffInit) {
+        initialized = true; // Initialization is now handled by server
+        console.debug("Initialization complete");
+    }
+
     // socket.write(Buffer.from([0x0d, 0x0a])); // Line Feed
     // from here on encryption is enabled, do not use socket.write() directly anymore
     // initialization complete
 
     await send("help");
     await delay(delayMs);
+    process.stdout.write("\r" + ANSI.bgRed + ANSI.white + "DEPRECATION NOTICE:" + ANSI.reset + " This is an unsupported version and might not work fully." + "\r\n");
     process.stdout.write("\rCtrl+X for client side commands\r\nCtrl+C to exit, Ctrl+D to force close\r\n> ");
     process.stdin.resume(); // Resume input
     // more commands can be added here
@@ -342,6 +449,28 @@ socket.on("connect", async () => {
 socket.on("end", () => {
     console.log("\nDisconnected from server");
     process.exit();
+});
+
+process.on('uncaughtException', (err) => {
+    if (err.code === 'ECONNRESET') {
+        console.warn("\nDisconnected from server");
+        process.exit();
+        } else if (err.code === 'ECONNREFUSED') {
+        console.warn("Connection refused");
+        process.exit();
+        } else if (err.code === 'ETIMEDOUT') {
+        console.warn("Connection timed out");
+        process.exit();
+        } else if (err.code === 'EHOSTUNREACH') {
+        console.warn("Host is unreachable");
+        process.exit();
+        } else if (err.code === 'ENETUNREACH') {
+        console.warn("Network is unreachable"); 
+        process.exit();
+    } else {
+        console.error('Unhandled exception:', err);
+        process.exit(1);
+    }
 });
 
 module.exports = {
